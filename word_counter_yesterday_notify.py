a109e9febf49d567c48ec99b4aa29498db641323
Mark Koester <mark@int3c.com>
2018-08-19 12:41:11 +0800

    resolves #1 prevent error if no data yesterday in daily stats notification script

--- word_counter_yesterday_notify.py
+++ word_counter_yesterday_notify.py
@@ -4,17 +4,26 @@ import time
 
 def add(x, y): return x + y
 
-#today = time.strftime("%Y-%m-%d")
-
 import datetime as DT
 today = DT.date.today()
 yesterdayRaw = today - DT.timedelta(days=1)
 yesterday = yesterdayRaw.strftime("%Y-%m-%d")
-# yesterday = DT.date.today.("%Y-%m-%d")
+
+# notification via osascript
+# TODO: Is there a better way to do this? 
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
 
 # read PLIST
 records = os.path.expanduser('~/Library/Application Support/WordCounter/app_records.plist')
 data = plistlib.readPlist(records)
+
+# check if wordcounter tracked yesterday
+if yesterday not in data:
+    notify("No wordcounter writing tracked yesterday")
+    exit()
 
 # collect per-app totals
 apps = data[yesterday]
@@ -27,12 +36,6 @@ for app in apps:
 total = reduce(add, counts)
 print total
 
-def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
-
 message = "%s Total Words Written Yesterday." % (total)
 notify("Word Count Yesterday", message)
-
 
